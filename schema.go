66315fc825fbf677137079e77a999e46b070979e
Jaap Groeneveld <jgroeneveld@fastmail.com>
2018-08-03 21:06:35 +0200

    some more godoc

--- schema.go
+++ schema.go
@@ -1,6 +1,55 @@
 // schema makes it easier to check if map/array structures match a certain schema. Great for testing JSON API's.
 //
-// For examples see https://github.com/jgroeneveld/schema/blob/master/README.md
+// Example:
+//
+//  func TestJSON(t *testing.T) {
+//      reader := getJSONResponse()
+//
+//      err := schema.MatchJSON(
+//          schema.Map{
+//              "id":       schema.IsInteger,
+//              "name":     "Max Mustermann",
+//              "age":      42,
+//              "height":   schema.IsFloat,
+//              "footsize": schema.IsPresent,
+//              "address":  schema.Map{
+//                  "street": schema.IsString,
+//                  "zip":    schema.IsString,
+//              },
+//              "tags": schema.ArrayIncluding("red"),
+//          },
+//          reader,
+//      )
+//
+//      if err != nil {
+//          t.Fatal(err)
+//      }
+//  }
+//
+// JSON Input
+//
+//  {
+//      "id": 12,
+//      "name": "Hans Meier",
+//      "age": 42,
+//      "height": 1.91,
+//      "address": {
+//          "street": 12
+//      },
+//      "tags": ["blue", "green"]
+//  }
+//
+// err.Error() Output
+//
+//  "address": Missing keys: "zip"
+//  "address.street": is no string but float64
+//  "name": "Hans Meier" != "Max Mustermann"
+//  "tags": red:string(0) not included
+//  Missing keys: "footsize"
+//
+// See https://github.com/jgroeneveld/schema for more examples.
+//
+// Also see https://github.com/jgroeneveld/trial for lightweight assertions.
 package schema
 
 import (
